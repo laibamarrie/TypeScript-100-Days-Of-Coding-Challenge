3728fedd39b8e4fbb4ec82f6d0d204841c9d4667
laibamarrie <irshadirshad706@gmail.com>
2024-04-16 18:00:55 +0500

    Added Day 2 Challenge

---
+++ Day 2/index.ts
@@ -0,0 +1,23 @@
+// 4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of it's author.
+// Your output should look like something like the following, including the quotation marks:
+// Albert Einstein once said ,"A person who never made a mistake never tried anything new."
+
+let quote: string ="A person who never made a mistake never tried anything new.";
+let author: string ="Albert Einstein";
+
+console.log(`${author} once said, "${quote}"`);
+
+// 5. Famous Quotes 2: Repeat exercise 4, but this time store the famous person's name in a variable called
+// famous person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famousPerson: string = "Albert Einstein";
+let Message: string = `${famousPerson} once said, "${quote}"`;
+
+console.log(Message);
+
+// 6. Stripping Names: Store a person's name,and include some whitespace characters at the beginning and end of the 
+// name. Make sure you use each character combination, "\t" and "\n",at least once.Print the name once,so the 
+// whitespace around the name is displayed. Then print the name after striping the white spaces.
+let otherpersonName = "\t\n   Laiba Marrie \n\t";
+console.log("original:", otherpersonName);
+console.log("stripped:", otherpersonName.trim());
