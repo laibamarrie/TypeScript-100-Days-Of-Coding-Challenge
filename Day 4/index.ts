f0d7b23a15c12822361d996af24f1185cbf8f539
laibamarrie <irshadirshad706@gmail.com>
2024-04-16 18:10:50 +0500

    Added Day 4 Challenge

---
+++ Day 4/index.ts
@@ -0,0 +1,35 @@
+// 10. Adding Comments: Choose two of the programs you've written,and add at least one comment to each.
+// If you don't have anything specific to write beacause your programs are too simple at this point,just
+// add your name and the current current date at the top of each program file.Then write one sentence
+// describing what the program does.
+
+//Author: [Laiba Marrie]
+//date: [Thursday, February 29 , 2024]
+
+//task 9: printing my favorite number.
+
+let otherfavoriteNumber: number = 16;
+
+//revealing my favorite number in a message formate.
+console.log (`My favourite number is ${otherfavoriteNumber}.`);
+
+// 11. Names: Store the names of a few friends in a array called names.Print each person's name
+// by accessing each element in the list,one at a time.tsc
+
+let names: string [] = ["Sara","Areeba","Ali","Hamza"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+
+// 12. Greetings: Start with the array you used in Exercise 11,but instead of just printing each person's name,
+// print a message to them. The text of each message should be the same,but each message should be personalized 
+// with the person's name.
+
+let othernames : string [] = ["Sara","Areeba","Ali","Hamza"];
+
+let message : string = "Do you like to play football?";
+console.log(othernames[0] + " " + message);
+console.log(othernames[1] + " " + message);
+console.log(othernames[2] + " " + message);
+console.log(othernames[3] + " " + message);
