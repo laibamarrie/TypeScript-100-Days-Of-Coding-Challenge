bd0f43f3979869ee9221365f41c483a7ac5b2574
laibamarrie <irshadirshad706@gmail.com>
2024-04-19 16:25:17 +0500

    Added Day 7 Challenge

---
+++ Day 7/index.ts
@@ -0,0 +1,21 @@
+// 19. Dinner Guests: Working with one of the programs from Exercise 14 through 18,print a message
+// indicating the number of people you are inviting to dinner.
+
+let invitations : string [] = ["Kathryn","Bilal"];
+let count_invitations : number = invitations.length;
+
+console.log(`${count_invitations} people will come to the dinner.`);
+
+// 20. Think of something you could store in a array:For Example,you could make a list of mountains,
+// rivers,countries,cities,languages, or anything else you'd like.Write a program that creates a 
+// list containing these items.
+
+let mountains: string[] = ["Mount Everest","Nanda Devi","Rakaposhi","Mount Elbrus","K2"];
+console.log("list of mountains");
+console.log(mountains);
+
+// 21. They think of something you could store in a Typescript Object.Write a program that creates Objects
+// containing these items.
+
+let person: {name: string, fname: string, age: number} = {name:"Laiba Marrie", fname: "female", age: 23};
+console.log(person);
