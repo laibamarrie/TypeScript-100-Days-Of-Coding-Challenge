cfecb68cab3c15034f47db45a324dff53a132287
laibamarrie <irshadirshad706@gmail.com>
2024-04-19 17:25:41 +0500

    Added Day 17 Challenge

---
+++ Day 17/index.ts
@@ -0,0 +1,28 @@
+// 49: Function with Rest Parameters: Write a function that takes a rest parameter representing multiple 
+ // hobbies.It should log each hobby with a statement saying you enjoy that hobby.
+
+ function enjoyHobbies (...hobbies: string [] ) {
+    for (const hobby of hobbies) {
+        console.log( ` I enjoy ${hobby}`);
+    }
+ }
+ 
+ enjoyHobbies ("coding" , "reading" , "watching TV" , "cooking");
+
+ // 50: Multiline Template Literals: Use template literals to create a multiline string that describes your
+ // ideal day.Include at least three different activities.
+
+ const idealDay = ` My Ideal day would start with prayer and a cup of tea.
+ In the afternoon , I would do some coding and then dive into a good book.
+ In the evening, after having dinner, I would watch TV with my loved ones and then follow my skincare routine. `
+ 
+ console.log(idealDay);
+
+ // 51: Refactoring to Arrow Functions: Take a simple function that calculates the area of a rectangle and
+ // refactor it into an arrow function.
+
+ function calculateArea (width: number, height: number):number {
+    return width * height;
+ }
+
+ const rectangleArea = (width: number, height: number) => width * height;
